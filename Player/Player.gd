ba3e01c8b90bc75ef42506e46d8d3d02e696571a
John <jastadj@gmail.com>
2018-04-07 11:13:54 -0700

    player loop

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,38 +1,106 @@
 extends Node2D
 
+
+const UP = Vector2(0,-1)
+const MAX_VEL = 200
+const GRAVITY = 15
+const ACCEL = 15
+const JUMP_FORCE = -425
+
+# references
 var mousePos
 var reticle
+var anim
+
+var facingRight = true
+var currentAnim = "idle"
+var vel = Vector2()
+var friction = false
 
 var snapshots = []
 
 func _ready():
-
+	
+	# get animation reference
+	anim = $AnimationPlayer
 	
 	# update camera reticle size
 	reticle = $Reticle/Reticle
 	reticle.updateReticleSize()
 	
+	# connect animation done signal
+	anim.connect("animation_finished", self, "updateAnimDone")
+	anim.play(currentAnim)
+	
 	set_process_input(true)
 	
 func _physics_process(delta):
 	
-	#move_and_slide(Vector2(0,50), Vector2(0,-1) )
-	pass
+	friction = false;
+	if !is_on_floor():
+		vel.y += GRAVITY
+	else:
+		vel.y = GRAVITY
+	
+	handleInput()
+	
+	move_and_slide(vel, UP)
+	
+	updateAnim()
+	
 
-func _process(delta):
+
+func handleInput():
 	
+	# move left
 	if Input.is_action_pressed("ui_left"):
-		move_and_slide(Vector2(-50,0), Vector2(0,-1) )
+		facingRight = false
+		vel.x = max(vel.x - ACCEL,-MAX_VEL)
+		currentAnim = "run"
+	
 	elif Input.is_action_pressed("ui_right"):
-		move_and_slide(Vector2(50,0), Vector2(0,-1) )
+		facingRight = true
+		vel.x = min(vel.x + ACCEL, MAX_VEL)
+		currentAnim = "run"
+	# else drag player to a half
+	else:
+		vel.x = lerp(vel.x, 0, 0.2)
+		friction = true
+		currentAnim = "idle"
 	
-	if Input.is_action_pressed("ui_down"):
-		move_and_slide(Vector2(0,50), Vector2(0,-1) )
-	elif Input.is_action_pressed("ui_up"):
-		move_and_slide(Vector2(0,-50), Vector2(0,-1) )
+	if is_on_floor():
+		if Input.is_action_just_pressed("ui_up"):
+				vel.y = JUMP_FORCE
+				currentAnim = "jumpstart"
+				anim.play(currentAnim)
+		if friction:
+			vel.x = lerp(vel.x, 0, 0.2)
+	#else player is in the air
+	else:
+		# if moving upwards
+		if vel.y < 0:
+			if currentAnim != "jumpstart":
+				currentAnim = "jump"
+		# else moving downards
+		else:
+			currentAnim = "fall"
+		
+		if friction:
+			vel.x = lerp(vel.x, 0, 0.05)
+
+func updateAnim():
 	
-	pass
+	if facingRight:
+		$Sprite.flip_h = false
+	else:
+		$Sprite.flip_h = true
 	
+	if anim.current_animation != currentAnim:
+		anim.play(currentAnim )
+
+func updateAnimDone(obj):
+	if currentAnim == "jumpstart":
+		currentAnim = "jump"
 
 func _input(event):
 	
